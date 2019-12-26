521140aec72d781a2a9a10b90693b3d3ef8579ed
G <gian.Libunao@reisystems.com>
2019-12-26 16:01:43 -0500

    Added controllers, Serializers and Models.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
 
+
+    def login
+        user = User.find_by(username: params[:username].downcase)
+        if (user && user.authenticate(params[:password]))
+            render json: UserSerializer.new(user).as_json
+        else
+            render json: {message: 'Login Error'}
+        end
+    end
+
     def view
         user = User.first()
         render json: UserSerializer.new(user).as_json
