f7d092855c6d206f6442b201f1c800f63c60c9d6
newtypev2 <gianlibunao@gmail.com>
2020-01-13 12:36:16 -0500

    update environment settings for dev

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://sugartracker.now.sh'
+    # origins 'https://sugartracker.now.sh'
+    origins '*'
 
     resource '*',
       headers: :any,
